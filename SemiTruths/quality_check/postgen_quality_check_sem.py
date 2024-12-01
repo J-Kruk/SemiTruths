f332a919c5825076c182623b92ee350792dfd602
Julia Kruk <jkruk3@nlp.cc.gatech.edu>
2024-12-01 17:06:02 -0500

    updating file structure with change metrics & QC scripts

---
+++ SemiTruths/quality_check/postgen_quality_check_sem.py
@@ -0,0 +1,124 @@
+import torch
+from PIL import Image
+from utils import *
+from tqdm import tqdm
+import pdb
+from tqdm import tqdm
+import csv
+import os
+import pandas as pd
+
+torch.set_default_tensor_type('torch.cuda.FloatTensor') 
+
+CSV_READ_FILE = "" # metadata for generated images
+
+column_map = {
+        "img_id": 0,
+        "perturbed_img_id": 1,
+        "original_caption": 2,
+        "perturbed_caption": 3,
+        "dataset": 4,
+        "diffusion_model": 5,
+        "language_model": 6,
+        "sem_magnitude": 7
+    }
+
+formats ={
+        "ADE20K": ".jpg",
+        "CelebAHQ": ".jpg",
+        "CityScapes": ".png",
+        "HumanParsing": ".png",
+        "OpenImages": ".jpg",
+        "SUN_RGBD": ".jpg",
+    }
+
+model_map = {
+    "OJ": "OpenJourney",
+    "SDv4": "StableDiffusion_v4",
+    "SDv5": "StableDiffusion_v5"
+}
+
+DS = 'SUN_RGBD'
+MODEL = "SDv5"
+LANG = "LlaVA-Hermes"
+
+PATH_TO_ORIG_DATA_PARENT = "" #path to original images 
+CSV_POSTGEN_QC =  "" #path to save postgen quality check results
+PATH_TO_PERTURB_DATA_PARENT = "" #path to perturbed images
+EDIT_EXTENSION = ".jpeg"
+DS_EXTENSION = formats[DS]
+
+def post_qual_check_row(row, writer):
+    if len(row[column_map['img_id']]):
+            
+        # remove model from orig path when just running independently
+        # orig_img_path = os.path.join(PATH_TO_DATA_PARENT, DS,model, row[column_map['img_id']]+DS_EXTENSION)
+        try:
+        # print("Calculating metrics for ", row[column_map['img_id']])
+            dataset = row[column_map['dataset']]
+            model = row[column_map['diffusion_model']]
+            language_model = row[column_map['language_model']]
+            if (dataset == DS) and (model == MODEL) and (language_model == LANG):
+                print("Calculating metrics for ", row[column_map['perturbed_img_id']])
+                orig_img_path = os.path.join(PATH_TO_ORIG_DATA_PARENT, dataset, row[column_map['img_id']]+DS_EXTENSION)
+                orig_img = Image.open(orig_img_path)
+                orig_caption = row[column_map['original_caption']]
+                brisque_score_orig = brisque_Score(orig_img)
+                #if perturbed image path does not exist, return:
+                
+                perturbed_img_path = os.path.join(PATH_TO_PERTURB_DATA_PARENT, language_model, dataset, model_map[model] , row[column_map['perturbed_img_id']])
+                # print(os.path.exists(perturbed_img_path))
+                # print(perturbed_img_path)
+                if not os.path.exists(perturbed_img_path):
+                    return
+                
+                perturbed_img = Image.open(perturbed_img_path)
+
+
+                # if not perturbed_img.getbbox():
+                #     return
+
+                perturbed_caption = row[column_map['perturbed_caption']]
+
+                cap2_img2 = calculate_image_caption_clip_similarity(perturbed_img , perturbed_caption)
+                direct_sim = calculate_directional_similarity(orig_img , orig_caption , perturbed_img , perturbed_caption)
+                img1_img2  = calculate_image_similarity(orig_img , perturbed_img)
+                brisque_score = brisque_Score(perturbed_img)
+
+                # print(cap2_img2, direct_sim, img1_img2, brisque_score_orig, brisque_score)
+                row.extend([cap2_img2, direct_sim, img1_img2, brisque_score_orig, brisque_score])
+
+                writer.writerow(row)
+                print("Metrics calculated for ", row[column_map['perturbed_img_id']])
+        except Exception as e:
+            print("Error in row: ", row)
+            print(e)
+        return
+            
+
+
+def postgen_quality_check(): 
+    f = open(CSV_READ_FILE, 'r')
+    file =  csv.reader(f)
+    header = next(file)
+
+    header_qc = header
+    header_qc.extend(['cap2_img2', 'direct_sim','img1_img2','brisque_score_orig','brisque_score_perturb'])
+
+    out_f = open(CSV_POSTGEN_QC, 'w')
+    writer = csv.writer(out_f)
+    writer.writerow(header_qc)
+    
+    i=0
+    for row in tqdm(file):
+        post_qual_check_row(row, writer)
+        
+    f.close()
+    out_f.close()
+
+def main():
+    postgen_quality_check()
+
+
+if __name__ == "__main__":
+    main()
---
+++ SemiTruths/quality_check/quality_check_quartiles.py
@@ -0,0 +1,106 @@
+import json
+import pdb
+import numpy as np
+from tqdm import tqdm
+
+DS = [
+    "ADE20K", 
+      "CelebAHQ", 
+      "CityScapes", 
+      "HumanParsing", 
+      "OpenImages", 
+      "SUN_RGBD"]
+       
+
+cap = []
+img = []
+
+
+DATA_FILE = f"/Users/mphute6/Documents/HalfTruths/postgen_quality_check_sem.csv"
+
+data = json.load(open(DATA_FILE, "r"))
+
+data_all = []
+
+for key in data.keys():
+    for edit in data[key]:
+        # pdb.set_trace()
+        try:
+            cap.append(edit["cap2_img2_similarity"])
+            img.append(edit["direct_similarity"])
+        except:
+            # print(edit)
+            continue
+
+cap = np.array(cap)
+img = np.array(img)
+
+new_data = {}
+
+# change range as per percentile value you want to check
+# it will return the similarity values that lie in the range
+# eg: (2,5), (90), (95), (95,100) etc
+# then we can check quality of generation and cross ref img based on path/ name
+values = range(2,5)
+desired_img_sim = [np.percentile(img,i) for i in values] 
+desired_cap_sim = [np.percentile(cap,i) for i in values]
+
+## SANDBOX TO FIND GOOD PERCENTILE RANGE
+for key in data.keys():
+    new_data[key] = []
+    for edit in data[key]:
+        # pdb.set_trace()
+        try:
+            # if edit["direct_similarity"]  in desired_img_sim:
+            if edit["cap2_img2_similarity"]  in desired_cap_sim:
+                print("Original:  ", edit["original_caption"])
+                print("Edited:  ", edit["edited_caption"])
+                print(key)
+                print("------------------------------")
+        except Exception as e:
+            # print(e)
+            continue
+
+
+## ACTUAL FILTERING
+
+# lower_cap = np.percentile(cap, 5)
+# upper_cap = np.percentile(cap, 90)
+
+# lower_img = np.percentile(img, 5)
+# upper_img = np.percentile(img, 100)
+# 
+# DATA_FILE = f"/Users/mphute6/Documents/HalfTruths/postgen_quality_check_sem.csv"
+# data = json.load(open(DATA_FILE, "r"))
+
+# for key in data.keys():
+#     for edit in data[key]:
+#         try:
+#             if edit["cap1_cap2_similarity"] < lower_cap or edit["cap1_cap2_similarity"] > upper_cap:
+#                 continue
+#             elif edit["img1_cap1_similarity"] < lower_img or edit["img1_cap1_similarity"] > upper_img:
+#                 continue
+#             else:
+#                 new_data[key] = new_data.get(key, [])
+#                 new_data[key].append(edit)
+#                 # pdb.set_trace()
+#         except:
+#             # print(edit)
+#             continue
+
+
+## SANITY CHECK
+# pdb.set_trace()
+# i = 0
+# for key in new_data.keys():
+#     for edit in new_data[key]:
+#         # print(edit)
+#         n = edit["cap1_cap2_similarity"]
+#         m = edit["img1_cap1_similarity"]
+#         i+=1
+
+# print(i)
+
+# OUT_DIR = f"/raid/mphute6/HalfTruths/SemanticDefinition/outputs/quality_check/{ds}.json"
+# with open(OUT_DIR, "w") as f:  
+#     json.dump(new_data, f)
